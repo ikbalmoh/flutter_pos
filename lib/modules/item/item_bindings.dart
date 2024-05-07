896c9a4386fa8f9c8f8e5469b7f97be22d4edc93
Ikbal <hikmat.iqbal@gmail.com>
2024-05-07 15:30:03 +0700

    remove getx module

--- lib/modules/item/item_bindings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:selleri/modules/item/item.dart';
-
-class ItemBindings implements Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => ItemController(ItemService()), fenix: true);
-  }
-}
--- lib/modules/outlet/outlet_bindings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:selleri/modules/outlet/outlet.dart';
-
-class OutletBindings implements Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => OutletController(OutletService()), fenix: true);
-  }
-}
