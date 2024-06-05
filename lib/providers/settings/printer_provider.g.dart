6819cd06d2bdda516a5ed334c1b188934602bee0
Ikbal <hikmat.iqbal@gmail.com>
2024-06-05 14:42:50 +0700

    core: permisison

---
+++ lib/providers/settings/printer_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'printer_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$printerNotifierHash() => r'ed3d5b7e018f5dc5ba0303f3d17e629d52b417dc';
+
+/// See also [PrinterNotifier].
+@ProviderFor(PrinterNotifier)
+final printerNotifierProvider =
+    AsyncNotifierProvider<PrinterNotifier, List<dynamic>>.internal(
+  PrinterNotifier.new,
+  name: r'printerNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$printerNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PrinterNotifier = AsyncNotifier<List<dynamic>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
