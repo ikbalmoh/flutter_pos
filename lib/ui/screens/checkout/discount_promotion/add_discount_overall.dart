53457a59fae4d11aae2753b985401e7b9ac5e6ae
Ikbal <hikmat.iqbal@gmail.com>
2024-08-12 15:19:34 +0700

    fix #517: lock landscape orientation on ipad

--- lib/ui/screens/checkout/discount_promotion/add_discount_overall.dart
+++ lib/ui/screens/checkout/discount_promotion/add_discount_overall.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:selleri/providers/cart/cart_provider.dart';
