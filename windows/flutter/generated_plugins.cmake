be674605825e9eb8519ee2d4a21dd264096d62c7
Ikbal Moh <hikmat.iqbal@gmail.com>
2025-02-28 11:20:03 +0700

    2.2.5: persistence device id

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   firebase_core
   flutter_secure_storage_windows
+  flutter_udid
   objectbox_flutter_libs
   permission_handler_windows
   print_bluetooth_thermal
