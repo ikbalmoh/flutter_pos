1afa3d10994789b7aeb59306876623b6a12bde1b
Ikbal Moh <hikmat.iqbal@gmail.com>
2025-08-28 14:20:22 +0700

    feat(auth): transform user, compan & outlet  model to use freezed

--- lib/features/auth/model/company.dart
+++ lib/features/auth/model/company.dart
@@ -1,58 +1,29 @@
 import 'dart:convert';
 
-class Company {
-  String idCompany;
-  String companyName;
-  String countryCode;
-  String? countryName;
-  int regionCode;
-  String? regionName;
-  int cityCode;
-  String? cityName;
-  String locale;
-  bool? statusConfigured;
-  String? companyEmail;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  Company({
-    required this.idCompany,
-    required this.companyName,
-    required this.companyEmail,
-    required this.countryCode,
-    this.countryName,
-    required this.regionCode,
-    this.regionName,
-    required this.cityCode,
-    this.cityName,
-    required this.locale,
-    this.statusConfigured,
-  });
+part 'company.freezed.dart';
+part 'company.g.dart';
 
-  Company.fromJson(Map<dynamic, dynamic> json)
-      : idCompany = json['id_company'],
-        companyName = json['company_name'],
-        companyEmail = json['company_email'],
-        countryCode = json['country_code'],
-        countryName = json['country_name'],
-        regionCode = json['region_code'],
-        regionName = json['region_name'],
-        cityCode = json['city_code'],
-        cityName = json['city_name'],
-        locale = json['locale'],
-        statusConfigured = json['status_configured'] ?? false;
+@freezed
+class Company with _$Company {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  factory Company({
+    required String idCompany,
+    required String companyName,
+    required String companyEmail,
+    required String countryCode,
+    String? countryName,
+    required int regionCode,
+    String? regionName,
+    required int cityCode,
+    String? cityName,
+    required String locale,
+    bool? statusConfigured,
+  }) = _Company;
 
-  Map<String, dynamic> toJson() => {
-        'id_company': idCompany,
-        'company_name': companyName,
-        'company_email': companyEmail,
-        'country_code': countryCode,
-        'country_name': countryName,
-        'region_code': regionCode,
-        'region_name': regionName,
-        'city_code': cityCode,
-        'city_name': cityName,
-        'locale': locale,
-        'status_configured': statusConfigured,
-      };
+  factory Company.fromJson(Map<String, dynamic> json) =>
+      _$CompanyFromJson(json);
 
   @override
   String toString() {
