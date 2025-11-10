4e304134c34f51c75ca7629dee4ae98a11997adf
Ikbal Moh <hikmat.iqbal@gmail.com>
2025-11-10 11:22:01 +0700

    select vehicle: redesign selectable customer vehicle

--- lib/features/customer/widget/customer_detail.dart
+++ lib/features/customer/widget/customer_detail.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,20 +7,39 @@ import 'package:selleri/features/customer/model/customer_vehicle.dart';
 import 'package:selleri/features/outlet/provider/outlet_provider.dart';
 import 'package:selleri/shared/utils/formater.dart';
 
-class CustomerDetail extends ConsumerWidget {
+class CustomerDetail extends ConsumerStatefulWidget {
   final Customer customer;
   final Function(Customer, {CustomerVehicle? vehicle}) onSelect;
   final Function(Customer) onEdit;
+  final bool isSelected;
+  final CustomerVehicle? vehicle;
 
   const CustomerDetail({
     required this.customer,
     required this.onSelect,
     required this.onEdit,
+    required this.isSelected,
+    required this.vehicle,
     super.key,
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _CustomerDetailState();
+}
+
+class _CustomerDetailState extends ConsumerState<CustomerDetail> {
+  CustomerVehicle? selectedVehicle;
+
+  @override
+  void initState() {
+    setState(() {
+      selectedVehicle = widget.vehicle;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final outletConfig = ref.watch(outletProvider).value as OutletSelected;
     List<String> customerMandatory =
         outletConfig.config.customMandatory?.customers ?? [];
@@ -33,7 +50,7 @@ class CustomerDetail extends ConsumerWidget {
         value != null && value.isNotEmpty
             ? ListTile(
                 contentPadding:
-                    const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
                 dense: true,
                 title: Text(title),
                 subtitle: Text(value),
@@ -46,137 +63,25 @@ class CustomerDetail extends ConsumerWidget {
               )
             : Container();
 
-    bool isExpired = customer.expiredDate != null
-        ? DateTimeFormater.stringToDateTime(customer.expiredDate!)!
+    bool isExpired = widget.customer.expiredDate != null
+        ? DateTimeFormater.stringToDateTime(widget.customer.expiredDate!)!
             .isBefore(DateTime.now())
         : false;
 
     Widget selectButton() {
-      ButtonStyle style = ButtonStyle(
-        overlayColor: WidgetStateProperty.all<Color>(
-            Colors.teal.shade200.withValues(alpha: 0.2)),
-        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
+      return TextButton.icon(
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.teal,
+          foregroundColor: Colors.white,
         ),
-        padding: WidgetStateProperty.all<EdgeInsets?>(
-            EdgeInsets.symmetric(horizontal: 17)),
+        onPressed: () =>
+            widget.onSelect(widget.customer, vehicle: selectedVehicle),
+        icon: const Icon(
+          Icons.check,
+          color: Colors.white,
+        ),
+        label: Text('select'.tr()),
       );
-
-      return vehicleEnabled &&
-              customer.vehicles != null &&
-              customer.vehicles!.isNotEmpty
-          ? MenuAnchor(
-              alignmentOffset: Offset(-120, -30),
-              useRootOverlay: true,
-              menuChildren: [
-                MenuItemButton(
-                  child: Text(
-                    'select_vehicle'.tr(),
-                    style: Theme.of(context)
-                        .textTheme
-                        .labelLarge
-                        ?.copyWith(color: Colors.grey.shade600),
-                  ),
-                ),
-                ...customer.vehicles!.map(
-                  (v) => MenuItemButton(
-                    onPressed: () => onSelect(customer, vehicle: v),
-                    style: style,
-                    leadingIcon: Icon(
-                      Icons.drive_eta_rounded,
-                      size: 22,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          v.licensePlate,
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        Text(
-                          v.vehicleType,
-                          style: Theme.of(context).textTheme.labelSmall,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                MenuItemButton(
-                  onPressed: () => onSelect(customer),
-                  style: style,
-                  leadingIcon: Icon(
-                    Icons.person,
-                    size: 22,
-                  ),
-                  child: Text('without_vehicle'.tr()),
-                ),
-                PopupMenuDivider(
-                  thickness: 0.2,
-                ),
-                MenuItemButton(
-                  onPressed: () => {},
-                  style: style.copyWith(
-                    iconColor:
-                        WidgetStateProperty.all<Color>(Colors.grey.shade600),
-                    foregroundColor:
-                        WidgetStateProperty.all<Color>(Colors.grey.shade600),
-                    overlayColor: WidgetStateProperty.all<Color>(
-                        Colors.red.shade200.withValues(alpha: 0.1)),
-                  ),
-                  leadingIcon: Icon(
-                    Icons.undo,
-                    size: 22,
-                  ),
-                  child: Text('cancel'.tr()),
-                ),
-              ],
-              builder: (context, controller, child) {
-                return TextButton.icon(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.teal,
-                    foregroundColor: Colors.white,
-                  ),
-                  onPressed: () {
-                    if (controller.isOpen) {
-                      controller.close();
-                    } else {
-                      controller.open();
-                    }
-                  },
-                  icon: const Icon(
-                    Icons.check,
-                    color: Colors.white,
-                  ),
-                  label: Text('select'.tr()),
-                );
-              },
-              style: MenuStyle(
-                backgroundColor: WidgetStateProperty.all<Color?>(Colors.white),
-                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                ),
-                padding:
-                    WidgetStateProperty.all<EdgeInsets?>(EdgeInsets.all(10)),
-                elevation: WidgetStateProperty.all<double>(15),
-              ),
-            )
-          : TextButton.icon(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.teal,
-                foregroundColor: Colors.white,
-              ),
-              onPressed: () => onSelect(customer),
-              icon: const Icon(
-                Icons.check,
-                color: Colors.white,
-              ),
-              label: Text('select'.tr()),
-            );
     }
 
     return SizedBox(
@@ -217,30 +122,74 @@ class CustomerDetail extends ConsumerWidget {
           ),
           Expanded(
             child: ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
+              // padding: const EdgeInsets.symmetric(horizontal: 15),
               shrinkWrap: true,
               children: [
-                listTile('customer_code'.tr(), customer.code.trim()),
-                listTile('customer_name'.tr(), customer.customerName.trim()),
-                listTile('group'.tr(), customer.groupNames),
-                listTile('Email', customer.email),
-                listTile('phone'.tr(), customer.phoneNumber),
-                listTile('address'.tr(), customer.address),
-                listTile('barcode'.tr(), customer.barcode),
-                listTile('expired_date'.tr(),
-                    customer.expiredDate != null ? customer.expiredDate! : '-'),
-                if (customer.vehicles?.isNotEmpty == true) ...[
-                  SizedBox(height: 12),
-                  Text('vehicle'.tr()),
-                  SizedBox(height: 5),
+                Column(
+                  children: [
+                    listTile('customer_code'.tr(), widget.customer.code.trim()),
+                    listTile('customer_name'.tr(),
+                        widget.customer.customerName.trim()),
+                    listTile('group'.tr(), widget.customer.groupNames),
+                    listTile('Email', widget.customer.email),
+                    listTile('phone'.tr(), widget.customer.phoneNumber),
+                    listTile('address'.tr(), widget.customer.address),
+                    listTile('barcode'.tr(), widget.customer.barcode),
+                    listTile(
+                        'expired_date'.tr(),
+                        widget.customer.expiredDate != null
+                            ? widget.customer.expiredDate!
+                            : '-'),
+                  ],
+                ),
+                if (widget.customer.vehicles?.isNotEmpty == true) ...[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 15, right: 15, top: 15, bottom: 8),
+                    child: Text(vehicleEnabled
+                        ? 'select_vehicle'.tr()
+                        : 'vehicle'.tr()),
+                  ),
                   ListView.builder(
                     shrinkWrap: true,
                     physics: NeverScrollableScrollPhysics(),
                     padding: const EdgeInsets.all(0),
                     itemBuilder: (context, idx) {
-                      final vehicle = customer.vehicles![idx];
+                      if (idx == 0 && vehicleEnabled) {
+                        return ListTile(
+                          onTap: () => setState(() {
+                            selectedVehicle = null;
+                          }),
+                          contentPadding:
+                              EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                          dense: false,
+                          title: Text('without_vehicle'.tr()),
+                          horizontalTitleGap: 10,
+                          leading: Icon(
+                            Icons.person,
+                            size: 20,
+                          ),
+                          trailing: Icon(
+                            selectedVehicle == null
+                                ? Icons.radio_button_checked
+                                : Icons.radio_button_off_rounded,
+                            size: 18,
+                            color: selectedVehicle == null
+                                ? Colors.teal
+                                : Colors.blueGrey,
+                          ),
+                          visualDensity: VisualDensity.compact,
+                          minVerticalPadding: 0,
+                          minTileHeight: 0,
+                        );
+                      }
+                      final vehicle = widget.customer.vehicles![idx - 1];
                       return ListTile(
-                        contentPadding: EdgeInsets.symmetric(vertical: 5),
+                        onTap: () => setState(() {
+                          selectedVehicle = vehicle;
+                        }),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 15),
                         dense: true,
                         title: Text(vehicle.licensePlate),
                         subtitle: Text(
@@ -248,14 +197,26 @@ class CustomerDetail extends ConsumerWidget {
                         horizontalTitleGap: 10,
                         leading: Icon(
                           Icons.drive_eta_rounded,
-                          size: 22,
+                          size: 20,
                         ),
+                        trailing: vehicleEnabled
+                            ? Icon(
+                                selectedVehicle == vehicle
+                                    ? Icons.radio_button_checked_rounded
+                                    : Icons.radio_button_off_rounded,
+                                size: 18,
+                                color: selectedVehicle == vehicle
+                                    ? Colors.teal
+                                    : Colors.blueGrey,
+                              )
+                            : null,
                         visualDensity: VisualDensity.compact,
                         minVerticalPadding: 0,
                         minTileHeight: 0,
                       );
                     },
-                    itemCount: customer.vehicles!.length,
+                    itemCount: widget.customer.vehicles!.length +
+                        (vehicleEnabled ? 1 : 0),
                   )
                 ],
               ],
@@ -281,7 +242,7 @@ class CustomerDetail extends ConsumerWidget {
                   style: TextButton.styleFrom(
                     foregroundColor: Colors.blue.shade700,
                   ),
-                  onPressed: () => onEdit(customer),
+                  onPressed: () => widget.onEdit(widget.customer),
                   icon: Icon(
                     Icons.edit,
                     color: Colors.blue.shade700,
