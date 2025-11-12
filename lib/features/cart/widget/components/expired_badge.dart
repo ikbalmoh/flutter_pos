141ff11ba3e71211784a0a893572913caa832117
Ikbal Moh <hikmat.iqbal@gmail.com>
2025-11-12 22:28:43 +0700

    feat(item variant): add expired date

--- lib/features/cart/widget/components/expired_badge.dart
+++ lib/features/cart/widget/components/expired_badge.dart
@@ -4,19 +4,27 @@ import 'package:flutter/material.dart';
 import 'package:selleri/shared/utils/formater.dart';
 
 class ExpiredBadge extends StatelessWidget {
-  const ExpiredBadge(
-      {super.key, required this.expiredDate, required this.isExpired});
+  const ExpiredBadge({
+    super.key,
+    required this.expiredDate,
+    required this.isExpired,
+    this.padding,
+    this.showLabel,
+  });
 
   final DateTime expiredDate;
   final bool isExpired;
+  final bool? showLabel;
+  final EdgeInsetsGeometry? padding;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(3)),
-      padding: const EdgeInsets.symmetric(
-        horizontal: 5,
-      ),
+      padding: padding ??
+          const EdgeInsets.symmetric(
+            horizontal: 5,
+          ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -31,8 +39,11 @@ class ExpiredBadge extends StatelessWidget {
           Text(
             isExpired
                 ? 'expired'.tr()
-                : DateTimeFormater.dateToString(expiredDate,
-                    format: 'dd MMM y'),
+                : [
+                    if (showLabel == true) 'expired'.tr(),
+                    DateTimeFormater.dateToString(expiredDate,
+                        format: 'dd MMM y')
+                  ].join(' '),
             style: Theme.of(context).textTheme.bodySmall?.copyWith(
                   color:
                       isExpired ? Colors.red.shade500 : Colors.orange.shade600,
